8d12fd7ee5354d0eb1ab66c64f9831dd4de39763
AidanDrakeBarrett <ab2576@byu.edu>
2024-09-20 11:01:04 -0600

    phase 0.6

--- shared/src/main/java/chess/ChessPiece.java
+++ shared/src/main/java/chess/ChessPiece.java
@@ -1,6 +1,8 @@
 package chess;
 
 import java.util.Collection;
+import java.util.HashSet;
+import java.util.Objects;
 
 /**
  * Represents a single chess piece
@@ -53,5 +55,43 @@ public class ChessPiece {
     public Collection<ChessMove> pieceMoves(ChessBoard board, ChessPosition myPosition) {
         throw new RuntimeException("Not implemented");
     }
+    private HashSet<ChessMove> pawnMoves(ChessBoard board, ChessPosition start) {
+        return null;
+    }
+    private void whiteDiags(ChessBoard board, ChessPosition start, HashSet<ChessMove> moves) {}
+    private void blackDiags(ChessBoard board, ChessPosition start, HashSet<ChessMove> moves) {}
+    private void pawnConfrontations(ChessPosition start, ChessPosition end, ChessBoard board, HashSet<ChessMove> moves) {}
+    private void pawnMoveAdder(ChessPosition start, ChessPosition end, HashSet<ChessMove> moves) {}
+    private HashSet<ChessMove> rookMoves(ChessBoard board, ChessPosition start) {
+        return null;
+    }
+    private HashSet<ChessMove> knightMoves(ChessBoard board, ChessPosition start) {
+        return null;
+    }
+    private HashSet<ChessMove> bishopMoves(ChessBoard board, ChessPosition start) {
+        return null;
+    }
+    private HashSet<ChessMove> queenMoves(ChessBoard board, ChessPosition start) {
+        return null;
+    }
+    private HashSet<ChessMove> kingMoves(ChessBoard board, ChessPosition start) {
+        return null;
+    }
+    private boolean moveAdder(ChessPosition start, ChessPosition end, ChessBoard board, HashSet<ChessMove> moves) {
+        return false;
+    }
+    private void pieceConfrontations(ChessPosition start, ChessPosition end, ChessBoard board, HashSet<ChessMove> moves) {}
 
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        ChessPiece that = (ChessPiece) o;
+        return color == that.color && type == that.type;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(color, type);
+    }
 }

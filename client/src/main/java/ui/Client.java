ed0d461a9f082978b12138b24af273dfbdee98c8
AidanDrakeBarrett <ab2576@byu.edu>
2024-11-14 14:23:49 -0700

    phase 5.5 more work on the client file.

--- client/src/main/java/ui/Client.java
+++ client/src/main/java/ui/Client.java
@@ -17,7 +17,31 @@ import static java.awt.Color.RED;
 
 
 public class Client {
-    public void run() {}
+    private final ServerFacade serverFacade;
+    private boolean loggedIn = false;
+    public Client(String url) {
+        serverFacade = new ServerFacade(url);
+    }
+    public void run() {
+        System.out.println("Welcome to my chess server. I don't know why you're still using cURL in 2024, but okay.\n");
+        System.out.println("Try typing 'help' for commands\n");
+        System.out.print(help());
+
+        Scanner scanner = new Scanner(System.in);
+        var result = "";
+        while(!result.equals("quit")) {
+            printPrompt();
+            String line = scanner.nextLine();
+            try {
+                result = eval(line);
+                System.out.print(result);
+            } catch (Throwable e) {
+                var msg = e.toString();
+                System.out.print(msg);
+            }
+        }
+        System.out.println();
+    }
     private void printPrompt() {
         System.out.print("\n\u001b[15;40;0m>>>");
     }
@@ -25,7 +49,7 @@ public class Client {
     public String help() {}
     public String eval(String input) {}
     public String register(String... params) throws ResponseException {}
-    public login(String... params) throws ResponseException {}
+    public String login(String... params) throws ResponseException {}
     public String create(String... params) throws ResponseException {}
     public String list() throws ResponseException {}
     public String join() throws ResponseException {}
